8f82dea9af9da9c6809c8c574a21a653b0074630
Angel Alvarez <angaalva683@gmail.com>
2024-02-20 01:38:59 -0600

    advance 1.3

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-st.header('Proyecto')
+car_data = pd.read_csv('/Users/angelgabrielalvarezvalenzuela/TripleTen-Project/vehicles_us.csv')
 
-st.write('Estamos en proceso') 
+st.header('SPRINT 5 - DATA ANALYST PROJECT')
+st.subheader('Creación de un histograma')
 
-car_data = pd.read_csv('/Users/angelgabrielalvarezvalenzuela/TripleTen-Project/vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-            # escribir un mensaje
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-            # crear un histograma
+hist_button = st.button('Construir Histograma')
+
+if hist_button:
+    st.write('Procesando...') 
     fig = px.histogram(car_data, x="odometer")
-        
-            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+st.subheader('Creación de un gráfico de dispersión')
+
+scatter_button = st.button('Construir Gráfico de Dispersión')
+
+if scatter_button:
+    st.write('Procesando...') 
+    fig = px.scatter(car_data, x="odometer")
     st.plotly_chart(fig, use_container_width=True)
