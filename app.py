c37536c8f5270ce104466ea9587495b196e51156
Angel Alvarez <angaalva683@gmail.com>
2024-02-20 02:27:28 -0600

    advance 1.5

--- app.py
+++ app.py
@@ -5,20 +5,31 @@ import streamlit as st
 car_data = pd.read_csv('/Users/angelgabrielalvarezvalenzuela/TripleTen-Project/vehicles_us.csv')
 
 st.header('SPRINT 5 - DATA ANALYST PROJECT')
-st.subheader('Creación de un histograma')
 
-hist_button = st.button('Construir Histograma')
+hist_price_button = st.button('Construir Histograma de Precio')
 
-if hist_button:
+if hist_price_button:
     st.write('Procesando...') 
-    fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    fig_price = px.histogram(car_data, x="price")
+    st.plotly_chart(fig_price, use_container_width=True)
 
-st.subheader('Creación de un gráfico de dispersión')
+hist_year_button = st.button('Construir Histograma de Año')
 
-scatter_button = st.button('Construir Gráfico de Dispersión')
-
-if scatter_button:
+if hist_year_button:
     st.write('Procesando...') 
-    fig = px.scatter(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    fig_year = px.histogram(car_data, x="year")
+    st.plotly_chart(fig_year, use_container_width=True)
+
+scatter_price_year_button = st.button('Construir Gráfico de Dispersión de Precio vs Año')
+
+if scatter_price_year_button:
+    st.write('Procesando...') 
+    fig_price_year = px.scatter(car_data, x="year", y="price")
+    st.plotly_chart(fig_price_year, use_container_width=True)
+
+scatter_price_odometer_button = st.button('Construir Gráfico de Dispersión de Precio vs Odómetro')
+
+if scatter_price_odometer_button:
+    st.write('Procesando...') 
+    fig_price_odometer = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig_price_odometer, use_container_width=True)
